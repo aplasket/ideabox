7cd424128be81ed97d963f5b2479c2dcbc6ab7cd
Ashley Plasket <ashley.plasket@gmail.com>
2024-01-10 12:09:41 -0500

    erasing and redoing for practice

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,28 @@
 import { useState } from 'react';
 import './App.css';
-import Ideas from '../Ideas/Ideas.js';
-import Form from '../Form/Form.js';
+import Form from '../Form/Form';
+import Ideas from '../Ideas/Ideas';
 
 function App(){
-  const [ideas, setIdeas] = useState([]);
+  const [ ideas, setIdeas ] = useState([]);
 
   function addIdea(newIdea){
     setIdeas([...ideas, newIdea]);
   }
 
   function deleteIdea(id){
-    setIdeas((ideas) => ideas.filter(idea => idea.id !== id));
+    setIdeas(ideas => ideas.filter(idea => idea.id !== id));
   }
 
   return (
-    <main className="App">
-      <h1>IdeaBox</h1>
-      <Form addIdea={addIdea} />
-      {!ideas.length && <h2>No ideas yet -- add some!</h2>}
+    <main className='App'>
+      <h1>Idea Box</h1>
+      <Form addIdea={addIdea}/>
+      {!ideas.length && <h2>No ideas yet, add some!</h2>}
       <Ideas ideas={ideas} deleteIdea={deleteIdea}/>
     </main>
   );
+
 }
 
 export default App;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,23 +1,25 @@
 import './Form.css';
 import { useState } from 'react';
 
-function Form ({addIdea}) {
+function Form({addIdea}){
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
-  function submitIdeas(event) {
+  function submitIdeas(event){
     event.preventDefault();
+    // create constant with current value of title and description, set id to current date
     const newIdea = {
       id: Date.now(),
       title,
       description
-    };
-
+    }
+    // call the addIdea method and pass in newIdea
     addIdea(newIdea);
+    //clear the input
     clearInput();
   }
 
-  function clearInput() {
+  function clearInput(){
     setTitle("");
     setDescription("");
   }
@@ -29,7 +31,7 @@ function Form ({addIdea}) {
         placeholder='Title'
         name='title'
         value={title}
-        onChange={event => setTitle(event.target.value)}
+        onChange= {event => setTitle(event.target.value)}
       />
 
       <input
